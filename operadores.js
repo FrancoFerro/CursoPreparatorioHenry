dd37a0dc18e5af7d41bebe63a20ca94f4634cd19
Franco Ferro <franco.ferro.it@hotmail.com>
2023-03-26 03:46:42 -0300

    Aprendiendo a programar

---
+++ operadores.js
@@ -0,0 +1,22 @@
+console.log(3 + 5);
+console.log(14 - 7);
+console.log(4 * 6);
+console.log(30 / 9);
+
+// Modulo o resto de una division
+
+console.log(95 % 4);
+
+// Precedencia de operadores
+
+console.log(3 + 5 * 2 - 8);
+console.log((3 + 5) * 2 - 8);
+
+// Adicion y strings
+
+console.log("Hola " + "Franco");
+
+// O lo podemos hacer creando variables
+var nombre = "Franco";
+var saludo = "Hola " + nombre;
+console.log(saludo);
